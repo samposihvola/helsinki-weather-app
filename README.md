3e5b981c0604c172ad2ac84a23f493effc41a18e
samposihvola <fytapes@proton.me>
2024-10-22 13:34:12 +0300

    Fetch data from API & present it in JSON

---
+++ README.md
@@ -0,0 +1,7 @@
+# Helsinki Weather App
+
+Python app that fetches data from yr.no API and presents in a readable format. Used through the command line.
+
+# Todo
+
+- 
---
+++ app.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def get_helsinki_weather():
+    # store contact info into a variable
+    sitename = 'https://github.com/samposihvola/helsinki-weather-app/tree/main'
+    # endpoint with helsinki coordinates
+    url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.192059&lon=24.945831'
+    # create useragent object to be used as credentials for the API
+    useragent = {
+        'User-Agent': sitename
+    }
+    response = requests.get(url, headers=useragent)
+    if response.status_code == 200:
+        data = response.json()
+        # convert data to a JSON formatted string with 4 spaces of intendation
+        data_str = json.dumps(data, indent=4)
+        return data_str
+    else:
+        print('failed to fetch content, response code:', response.status_code)
+
+if __name__ == '__main__':
+    get_helsinki_weather()
