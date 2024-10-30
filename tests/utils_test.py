5727cb77abd4fb25a85243d77a86ce194b45ee33
samposihvola <fytapes@proton.me>
2024-10-30 10:09:52 +0200

    Unit test for the next_1_hours branch

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,39 +1,31 @@
 import pytest
 import sys
 import os
+import json
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utils import get_helsinki_weather
+from utils import get_helsinki_weather, get_weather_data
 
 def test_get_helsinki_weather_error_branch(mocker):
-    mock_response = mocker.Mock()
-    mock_response.status_code = 404    
-    mocker.patch('requests.get', return_value=mock_response)
+  # create a mock response object with a 404 status code to simulate an error from the API
+  mock_response = mocker.Mock()
+  mock_response.status_code = 404
+  # return the mock response instead of an actual HTTP request
+  mocker.patch('requests.get', return_value=mock_response)
 
-    with pytest.raises(Exception) as excinfo:
-      get_helsinki_weather()
-    
-    assert 'failed to fetch content, response code 404' in str(excinfo.value)
+  # expect an exception due to the 404 response
+  with pytest.raises(Exception) as excinfo:
+    get_helsinki_weather()
+  
+  assert 'failed to fetch content, response code 404' in str(excinfo.value)
 
 def test_get_weather_data_next_1_hours_branch(mocker):
-  mock_data =  {
-    "properties": {
-      "timeseries": [{
-        "time": "2024-10-30T07:00:00Z",
-        "data": {
-          "instant": {
-            "details": {
-              "air_temperature": 8.4,
-            }
-          },
-          "next_1_hours": {
-              "summary": {
-              "symbol_code": "fair_day"
-              },
-              "details": {
-              "precipitation_amount": 0
-              }
-          },
-        }
-      }]
-    }
-  }
+  with open('mock_data.json', 'r') as file:
+     mock_data = json.load(file)
+
+  # create mock data for get_helsinki_weather function to return
+  mocker.patch('utils.get_helsinki_weather', return_value=mock_data)
+
+  result = get_weather_data()
+  # expect the result to be the symbol_code in the mock_data.json file under 'next_1_hours'
+  expected_result = 'fair_day'
+  assert result[0]['details'] == expected_result
